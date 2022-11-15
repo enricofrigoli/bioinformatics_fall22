b388a0d24197053b86f3fb0d614c3e782c5960c3
chres98 <117674911+chres98@users.noreply.github.com>
2022-11-15 11:17:24 +0100

    Update task2.py
    update paths
    update alignment algorithm
    test with muromonab, bevacizumab and caplacizumab

--- task2.py
+++ task2.py
@@ -1,48 +1,68 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  2 13:47:54 2022
-
-@author: lotte
-"""
 import Bio.SeqIO as SeqIO
 from Bio import pairwise2
+import os
 from datetime import datetime
 
-humDir = r"C:\Users\lotte\Documents\FAU\BioInf\human.fa"
-mouseDir = r"C:\Users\lotte\Documents\FAU\BioInf\mouse.fa"
+# get the directories
+# used files have to be stored in same directory as the code
+cwd = os.getcwd()
+humDir = os.path.join(cwd,"human.fa")
+mouseDir = os.path.join(cwd, "mouse.fa")
+muromonab = os.path.join(cwd, "muromonab.fasta")
+bevacizumab = os.path.join(cwd, "bevacizumab.fasta")
+caplacizumab = os.path.join(cwd, "caplacizumab.fasta")
 
-muromonab = SeqIO.to_dict(SeqIO.parse(r"C:\Users\lotte\Documents\FAU\BioInf\muromonab.fasta", "fasta"))
-muromonabSeq = (muromonab['muromonab']).seq
+# get muromonab sequence
+muromonab = SeqIO.to_dict(SeqIO.parse(muromonab, "fasta"))
+muromonab = list(muromonab.values())
+muromonab = muromonab[0].seq
 
-# bevacizumab, caplacizumab
+# get bevacizumab sequence
+bevacizumab = SeqIO.to_dict(SeqIO.parse(bevacizumab, "fasta"))
+bevacizumab = list(bevacizumab.values())
+bevacizumab = bevacizumab[0].seq
 
-def humanization_model(seq,humDir,mouseDir):
+# get caplacizumab sequence
+caplacizumab = SeqIO.to_dict(SeqIO.parse(caplacizumab, "fasta"))
+caplacizumab = list(caplacizumab.values())
+caplacizumab = caplacizumab[0].seq
+
+
+# local alignment of given sequence with all sequences given sequences of humans and mice
+# use global alignment if sequences have roughly same length else use local
+def alignment(seq,humDir,mouseDir):
     print(datetime.now())
+    
+    # get human and mouse dicts
     humDict = SeqIO.to_dict(SeqIO.parse(humDir, "fasta"))
     mouseDict = SeqIO.to_dict(SeqIO.parse(mouseDir, "fasta")) 
     
+    # get sequences
     humSequences = [s.seq for s in humDict.values()]
     mouseSequences = [s.seq for s in mouseDict.values()]
     
+    # start alignment for all given human sequences
     print('starting pairwise alignment human dataset\n')    
-    humAlignments = [pairwise2.align.globalxx(seq,humS) for humS in humSequences]
-    print("done pairwise alignment human dataset\n")
-   
+    humAlignments = [pairwise2.align.localxx(seq,humS) for humS in humSequences]
+    print("done pairwise alignment human dataset\n")  
     print(datetime.now())
     
+    # start alignment for all given mouse sequences
     print('starting pairwise alignment mouse dataset\n')
-    mouseAlignments = [pairwise2.align.globalxx(seq,mouseS) for mouseS in mouseSequences]
-    print("done pairwise alignment mouse dataset\n")
-    
+    mouseAlignments = [pairwise2.align.localxx(seq,mouseS) for mouseS in mouseSequences]
+    print("done pairwise alignment mouse dataset\n")    
     print(datetime.now())
          
     return humAlignments, mouseAlignments
 
+# find the highest score of all possible alignments
 def get_highest_score(humAlignments,mouseAlignments):
 
     hMax = 0
     mMax = 0
-        
+      
+    # find highest score of human alignments and highest score of mouse alignments
     for hA,mA in zip(humAlignments,mouseAlignments):
         for align in hA:
             if align.score > hMax: 
@@ -52,14 +72,21 @@ def get_highest_score(humAlignments,mouseAlignments):
             if align.score > mMax:
                 mMax = align.score
                 mSeq = align.seqB
-
+    
+    # compare human and mouse highscore and return highest
     if hMax > mMax:
         return {'dataset': 'human', 'seq': hSeq, 'score': hMax}
     return {'dataset': 'mouse', 'seq': mSeq, 'score': mMax}        
 
-# h,m = humanization_model(testSeq, humDir, mouseDir)
-# high = get_highest_score(h, m)
 
-hMuromonabSeq, mMuromonabSeq = humanization_model(muromonabSeq, humDir, mouseDir)
+hMuromonabSeq, mMuromonabSeq = alignment(muromonab, humDir, mouseDir)
 highMuromonabSeq = get_highest_score(hMuromonabSeq, mMuromonabSeq)
-print(highMuromonabSeq)
+print("highest score for alignment with muromonab: "+ str(highMuromonabSeq))
+
+hBevacizumabSeq, mBevacizumabSeq = alignment(bevacizumab, humDir, mouseDir)
+highBevacizumabSeq = get_highest_score(hBevacizumabSeq, mBevacizumabSeq)
+print("highest score for alignment with bevacizumab: " + str(highBevacizumabSeq))
+
+hCaplacizumabSeq, mCaplacizumabSeq = alignment(caplacizumab, humDir, mouseDir)
+highCaplacizumabSeq = get_highest_score(hCaplacizumabSeq, mCaplacizumabSeq)
+print("highest score for alignment with caplacizumab: " + str(highCaplacizumabSeq))
